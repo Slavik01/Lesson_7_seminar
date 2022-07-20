53e376cfe925f89d90509182c8dce60af583f974
Slavik <slavik-skripka@yandex.ru>
2022-07-20 13:32:06 +0300

    1 commit

---
+++ Homework_7_seminar/Three/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:З
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int [,] matrix = new int [2, 3];
+double sum = 0;
+double result = 0;
+for (int i = 0; i < matrix.GetLength(0); i++) // проход по 1 строчке 
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] =  new Random().Next(0,11);
+        Console.Write(matrix[i,j] + "\t");
+        sum += matrix[i,j]; // считает 00 01 02
+    }
+    result = sum /3; // вынесли из for что бы не делил на каждый индекс 
+    sum = 0; // не тянуть на 2 круг значение 
+    Console.WriteLine($"Cреднее арефмитическое чисел равна: {result}");
+}
+
+
+
+
+// int [,] matrix = new int [3, 3];
+
+// void FillArrayRandomNumbers (int[,] matrix)
+// {
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] =  new Random().Next(0,11);
+//             Console.Write(matrix[i,j] + "\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// FillArrayRandomNumbers(matrix);
+
+
---
+++ Homework_7_seminar/Two/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.Write("Введите строку: ");
+int rows = Int32.Parse(Console.ReadLine());
+Console.Write("Введите столбец: ");
+int columns = Int32.Parse(Console.ReadLine());
+int rowsTest = 0; // индекс на 1 всегда больше 
+int columnsTest = 0;// размер таблици 3 на 3 будет 0 1 2 
+int [,] matrix = new int[rows,columns];  // при 10,10 переменные не нужны
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(0,11);
+        Console.Write(matrix[i,j] + "\t");
+        if (rowsTest == i && columnsTest == j) // сравниваем с элементом строки и столбца
+        {
+            Console.Write($"Числа найденны: строчка {rowsTest}, столбец {columnsTest}");
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Элемента нет");
+        }
+    }
+
+    Console.WriteLine();
+}
